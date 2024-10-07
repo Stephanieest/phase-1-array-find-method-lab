a3f3caa5947bbe64f2177f6375b89248c8152e1a
Stephanie <stephnjonjo@gmail.com>
2024-10-07 13:30:16 +0300

    adds new codes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(records) {
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+    ] 
+ const winRecord = records.find(record => record.result === "W");
+    
+    if (winRecord) {
+        return winRecord.year;
+    }
+    
+    return undefined;
+}
 // code your solution here
